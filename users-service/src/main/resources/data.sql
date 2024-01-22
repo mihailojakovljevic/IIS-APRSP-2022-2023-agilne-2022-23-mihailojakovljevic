4867f5fef4aa24f66200dbde02758956a4b2ce7e
jakovljevicm <mihailo.jakovljevic@gmail.com>
2024-01-22 22:23:11 +0100

    first commit

---
+++ users-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into custom_user(id,email,password,role)
+values(1,'vladimir_fabri98@uns.ac.rs','password','ADMIN');
+
+insert into custom_user(id,email,password,role)
+values(2,'user1@gmail.com','user1','USER');
+
+insert into custom_user(id,email,password,role)
+values(3,'user2@gmail.com','user2','USER');
+
+insert into custom_user(id,email,password,role)
+values(4,'mihailo.jakovljevic@gmail.com','password123','OWNER');
